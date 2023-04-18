fec28be05333182ed1b6fd09af5217f7ef369c7a
CLoveYC <100456466+CLoveYC@users.noreply.github.com>
2023-04-19 00:36:46 +0800

    Update Criterium.cc

--- Criterium.cc
+++ Criterium.cc
@@ -1,67 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double circle_tire = 1.1;
-double lap_tire = 1.2;
-
-double sigma( int n , int lap )
+double best_time = INT_MAX;
+vector<int> ans;
+void findCombinationsUtil(int arr[], int index, int num, int reducedNum, int numPeo, vector<int>& t)
 {
-	double total_time = 0;
-	for( int i=1 ; i!=n+1 ; i++ )
-		total_time += pow( circle_tire , i-1 )*pow(lap_tire , lap-1);
-	return total_time;
+
+	if (reducedNum < 0)
+		return;
+
+	if (reducedNum == 0 && index >= numPeo)
+	{
+		double time = 0;
+		vector<int> temp;
+		
+		for( int i = 0; i < index; i++)
+			temp.push_back(arr[i]); 
+		reverse( temp.begin() , temp.end() );
+//		for( int i=0 ; i<index ; i++ ) cout << temp[i] << " ";
+//		cout << endl;
+//		cout << index << endl;
+		for( int cir = 0 ; cir < index ; cir++ )
+			for(int i=0 ; i<temp[cir] ; i++)
+			{
+				time += t[cir%numPeo] * pow(1.1 , i) * pow(1.2, cir/numPeo);
+//				cout << time << " " << t[cir%3] << " " << pow(1.1 , i) << " " << pow(1.2, cir/numPeo) << endl;
+			}
+//		cout << time << endl;
+		if( time < best_time )
+		{
+			best_time = time;
+			ans.clear();
+			for(int i=0 ; i<index ; i++)
+				ans.push_back(temp[i]);
+		}
+		return;
+	}
+
+	int prev = (index == 0)? 1 : arr[index-1];
+
+	for (int k = prev; k <= num ; k++)
+	{
+		arr[index] = k;
+		
+		findCombinationsUtil(arr, index + 1, num, reducedNum - k , numPeo, t);
+	}
+}
+
+void findCombinations(int n , int numPeo, vector<int>& t)
+{
+	int arr[n];
+
+	findCombinationsUtil(arr, 0, n, n, numPeo, t);
 }
 
 int main()
 {
-
-	int n;
-	int best_lap = 1;
-	cin >> n;
-	for( int lap = 1 ; lap <= n ; lap++ ) // if lap = 2, n = 1 1
-	{
-		double temp = sigma( n , lap );
-		cout << temp << " ";
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+ 
+    int n, numPeo = 0;
+    cin >> n >> numPeo;
+    vector<int> t;
+    t.resize(numPeo,0);
+    for(int i = 0 ; i < numPeo ; i++ )	cin >> t[i];
+    
+	findCombinations(n, numPeo, t );
+	
+	cout << best_time << endl;
+	for( vector<int>::iterator i = ans.begin() ; i != ans.end() ; i++ ) cout << *i << " ";
+ 
+    return 0;
 }
-
-
-
-
-
-
-
-//#include <iostream>
-//#include <cmath>
-//#include <climits>
-//
-//using namespace std;
-//
-//int main() {
-//    int n;
-//    double circle_tire_ratio, lap_tire_ratio, lap_time;
-//    cin >> n >> circle_tire_ratio >> lap_tire_ratio >> lap_time;
-//
-//    double best_time = INT_MAX;
-//    int best_m = 1;
-//
-//    for (int m = 1; m <= n; m++) {
-//        double total_time = 0;
-//        for (int i = 1; i <= m; i++) {
-//            double time = lap_time * pow(circle_tire_ratio, i - 1);
-//            total_time += time;
-//        }
-//        if (total_time < best_time) {
-//            best_time = total_time;
-//            best_m = m;
-//        }
-//    }
-//
-//    cout << "Best m: " << best_m << endl;
-//    cout << "Best time: " << best_time << endl;
-//    return 0;
-//}
-// n = 2
-// best_m = 1
-// 2 -> 
-// 1 1
+ 
