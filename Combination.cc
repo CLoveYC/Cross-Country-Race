f1eb764e50cef4bd510c43657e56ca115c4dd417
CLoveYC <100456466+CLoveYC@users.noreply.github.com>
2023-04-19 09:06:29 +0800

    Update Combination.cc

--- Combination.cc
+++ Combination.cc
@@ -1,20 +1,12 @@
-// C++ program to find out all combinations of
-// positive numbers that add upto given number
 #include<bits/stdc++.h>
 using namespace std;
 
-/* arr - array to store the combination
-	index - next location in array
-	num - given number
-	reducedNum - reduced number */
 void findCombinationsUtil(int arr[], int index, int num, int reducedNum, int numPeo)
 {
-	// Base condition
 	if (reducedNum < 0)
 		return;
 
-	// If combination is found, print it
-	if (reducedNum == 0 && index >= numPeo && numPeo+2 >= index )
+	if (reducedNum == 0 && index >= numPeo)
 	{
 		vector<int> temp;
 		for( int i = 0; i < index; i++)
@@ -26,36 +18,24 @@ void findCombinationsUtil(int arr[], int index, int num, int reducedNum, int num
 		return;
 	}
 
-	// Find the previous number stored in arr[]
-	// It helps in maintaining increasing order
 	int prev = (index == 0)? 1 : arr[index-1];
 
-	// note loop starts from previous number
-	// i.e. at array location index - 1
 	for (int k = prev; k <= num ; k++)
 	{
-		// next element of array is k
 		arr[index] = k;
 		
-		// call recursively with reduced number
 		findCombinationsUtil(arr, index + 1, num, reducedNum - k , numPeo);
 	}
 }
 
-/* Function to find out all combinations of
-positive numbers that add upto given number.
-It uses findCombinationsUtil() */
 void findCombinations(int n , int numPeo)
 {
-	// array to store the combinations
-	// It can contain max n elements
 	int arr[n];
 
-	//find all combinations
 	findCombinationsUtil(arr, 0, n, n, numPeo);
 }
 
-// Driver code
+
 int main()
 {
 	int n, numPeo;
